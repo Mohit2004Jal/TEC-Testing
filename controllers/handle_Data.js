fc8f856eb24ff0029fa922529bea53ca451c6e31
MOHIT DAVAR <mohitdavar2004@gmail.com>
2024-10-15 15:02:46 +0530

    CHechking graph

--- controllers/handle_Data.js
+++ controllers/handle_Data.js
@@ -1,7 +1,20 @@
+const axios = require('axios');
 const { analyzeFuelData } = require("../utils/analyse_Fuel_Data")
 // Object to store fuel data and alert statuses for each device
 const deviceData = {};
 
+async function PostData(data, longitude, latitude) {
+    try {
+        const response = await axios.post('http://thingsboard.cloud/api/v1/39fwmmulx00ivunhco6t/telemetry', {
+            fuel: data,
+            longitude: longitude,
+            latitude: latitude
+        });
+        console.log('POST Response Data:', response.data);
+    } catch (error) {
+        console.error('Error posting data:', error);
+    }
+}
 const handleData = (req, res) => {
     const deviceId = req.params.id;
     let { fuel, longitude, latitude } = req.body
@@ -35,6 +48,7 @@ const handleData = (req, res) => {
         console.log(`Device ${deviceId} - Fuel Data: `, { fuel, latitude, longitude });
         // */
         analyzeFuelData(deviceId, longitude, latitude, deviceData);
+        PostData(fuel, longitude, latitude);
         res.status(200).send(`Fuel data received successfully for device ${deviceId}`);
     } else {
         res.status(400).send("Invalid fuel data");
